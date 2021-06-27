e9c7239fb9620f02f799b7a5c6fced72ca0da5ad
Cameron Prybol <cameron.prybol@gmail.com>
2021-06-27 09:31:27 -0400

    Jaccard-based reconstruction accuracy metrics implemented

--- src/Mycelia.jl
+++ src/Mycelia.jl
@@ -3015,4 +3015,159 @@ end
     end
 end
 
+
+function simple_polish_fastq(simple_kmer_graph, fastq_file; min_depth=3)
+    # min_depth = 3
+    solid_vertices = filter(v -> simple_kmer_graph.vprops[v][:weight] >= min_depth, LightGraphs.vertices(simple_kmer_graph))
+    filtered_simple_kmer_graph, vertex_map = LightGraphs.induced_subgraph(simple_kmer_graph, solid_vertices)
+    kmers = [filtered_simple_kmer_graph.vprops[v][:kmer] for v in LightGraphs.vertices(filtered_simple_kmer_graph)]
+    k = filtered_simple_kmer_graph.gprops[:k]
+    
+    polished_fastq_file = replace(fastq_file, ".fastq" => ".k$k.d$(min_depth).fastq")
+
+    transition_probabilities = Mycelia.initialize_transition_probabilities(filtered_simple_kmer_graph)
+    state_likelihoods = [Float64(filtered_simple_kmer_graph.vprops[v][:weight]) for v in LightGraphs.vertices(filtered_simple_kmer_graph)]
+    state_likelihoods ./= sum(state_likelihoods)
+
+    fastq_reader = FASTX.FASTQ.Reader(open(fastq_file))
+    fastq_writer = FASTX.FASTQ.Writer(open(polished_fastq_file, "w"))
+
+    for fastx_record in fastq_reader
+        bubble_start = 0
+        updated_path = Vector{Pair{Int, Bool}}()
+    #     @show FASTX.sequence(fastx_record)
+        for (i, kmer) in enumerate(BioSequences.each(BioSequences.BigDNAMer{k}, FASTX.sequence(fastx_record)))
+            canonical_kmer = min(kmer.fw, kmer.bw)
+            orientation = canonical_kmer == kmer.fw
+            kmer_index_range = searchsorted(kmers, canonical_kmer)
+            kmer_is_solid = !isempty(kmer_index_range)
+    #         @show kmer_is_solid
+            if kmer_is_solid
+                kmer_index = first(kmer_index_range)
+            else
+                kmer_index = 0
+            end
+
+            in_bubble = bubble_start > 0
+
+            if !kmer_is_solid
+                if !in_bubble
+    #                 @show "starting a bubble"
+                    bubble_start = i
+                else
+    #                 @show "continuing in a bubble"
+                end
+            else
+                if !in_bubble
+    #                 @show "pushing solid kmer to updated path"
+                    push!(updated_path, kmer_index => orientation)
+                else
+                    if bubble_start == 1
+    #                     @show "ending an opening bubble"
+                        # we're in a bubble that began at the beginning of the read
+                        # we'll do nothing and just remove this
+                        # equivalent to tip clipping
+    #                     @show "pushing solid kmer to updated path"
+                        push!(updated_path, kmer_index => orientation)
+                        bubble_start = 0
+                    else
+    #                     @show "found end of an anchored bubble -- correcting"
+                        source_vertex, source_orientation = last(updated_path)
+                        destination_vertex, destination_orientation = kmer_index, orientation                
+
+                        shortest_paths = LightGraphs.yen_k_shortest_paths(
+                            filtered_simple_kmer_graph,
+                            source_vertex,
+                            destination_vertex,
+                            LightGraphs.weights(filtered_simple_kmer_graph),
+                            3).paths
+
+                        if isempty(shortest_paths)
+                            error("no valid alternate paths found")
+                        end
+                        candidate_path_probabilities = ones(length(shortest_paths))
+                        oriented_candidate_paths = [
+                            [last(updated_path)] for i in 1:length(shortest_paths)
+                        ]
+
+                        for (i, candidate_path) in enumerate(shortest_paths)
+                            for dest_vertex in candidate_path[2:end]
+                                source_vertex, source_orientation = last(oriented_candidate_paths[i])
+                                candidate_path_probabilities[i] *= transition_probabilities[source_orientation][source_vertex, dest_vertex]
+                                candidate_path_probabilities[i] *= state_likelihoods[dest_vertex]
+                                if candidate_path_probabilities[i] > 0
+                                    edge = LightGraphs.Edge(source_vertex, dest_vertex)
+                                    destination_orientation = 
+                                    first(
+                                        filter(o -> o.source_orientation == source_orientation,
+                                            filtered_simple_kmer_graph.eprops[edge][:orientations])).destination_orientation
+                                    push!(oriented_candidate_paths[i], (dest_vertex => destination_orientation))
+                                else
+                                    break # this path is no good, evaluate the next
+                                end
+                            end
+                        end
+                        non_zero_indices = findall(p -> p > 0, candidate_path_probabilities)
+                        if isempty(non_zero_indices)
+                            error("no valid alternate path probabilities")
+                        end
+
+                        candidate_path_probabilities = candidate_path_probabilities[non_zero_indices]
+                        oriented_candidate_paths = oriented_candidate_paths[non_zero_indices]
+
+                        # offset is for debugging
+                        # make sure that anchors on both sides are the same
+                        offset = 0
+                        observed_sequence = FASTX.sequence(fastx_record)[bubble_start+k-1-offset:i-1+offset]                    
+                        for (i, oriented_candidate_path) in enumerate(oriented_candidate_paths)
+                            candidate_sequence = Mycelia.oriented_path_to_sequence(
+                                filtered_simple_kmer_graph, 
+                                oriented_candidate_path)
+                            candidate_sequence = candidate_sequence[k+1-offset:end-k+offset]
+                            alignment_result = BioAlignments.pairalign(
+                                BioAlignments.LevenshteinDistance(),
+                                candidate_sequence,
+                                observed_sequence)
+#                             @show alignment_result
+    #                         @show alignment_result
+                            average_error_rate = Statistics.mean(Mycelia.q_value_to_error_rate.(FASTX.quality(fastx_record)))
+                            for error in 1:alignment_result.value
+                                candidate_path_probabilities[i] *= average_error_rate
+                            end
+                            for match in 1:BioAlignments.count_matches(alignment_result.aln)
+                                candidate_path_probabilities[i] *= (1 - average_error_rate)
+                            end
+                        end
+
+                        chosen_replacement = StatsBase.sample(oriented_candidate_paths, StatsBase.weights(candidate_path_probabilities))
+
+                        for i in 2:length(chosen_replacement)
+                            oriented_state = chosen_replacement[i]
+                            push!(updated_path, oriented_state)
+                        end
+                        bubble_start = 0
+                    end
+                end
+            end
+        end
+    #     @show updated_path
+        sequence = Mycelia.oriented_path_to_sequence(filtered_simple_kmer_graph, updated_path)
+        alignment_result = BioAlignments.pairalign(
+            BioAlignments.LevenshteinDistance(),
+            sequence,
+            FASTX.sequence(fastx_record))
+#         if alignment_result.value > 0
+# #             @show alignment_result
+#         end
+        quality = StatsBase.sample(FASTX.quality(fastx_record), length(sequence), replace=true, ordered=true)
+        description =  join(filter(!isempty, (FASTX.description(fastx_record), "k$k.d$(min_depth)")), '.')
+        identifier = FASTX.identifier(fastx_record)
+        new_record = FASTX.FASTQ.Record(identifier, description, sequence, quality)
+        write(fastq_writer, new_record)
+    end
+    close(fastq_reader)
+    close(fastq_writer)
+    return polished_fastq_file
+end
+
 end # module
