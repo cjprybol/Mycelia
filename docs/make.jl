020c2626f276e5ba6dc2554d971eaa2fc4002197
Cameron Prybol <cameron.prybol@gmail.com>
2021-12-17 16:03:03 +0000

    Big docs revamp to handle file already exists errors

--- docs/make.jl
+++ docs/make.jl
@@ -7,36 +7,31 @@ import DataStructures
 
 PKG_BASE = dirname(dirname(pathof(Mycelia)))
 
+mkpath("$(PKG_BASE)/docs/src")
 # copy readme from repo to being main index file in documentation
 cp("$(PKG_BASE)/README.md", "$(PKG_BASE)/docs/src/index.md", force=true)
 # for svg in filter(x -> occursin(r"\.svg$", x), readdir(PKG_BASE, join=true))
 #     cp(svg, "$(PKG_BASE)/docs/src/$(basename(svg))", force=true)
 # end
 
-pages = DataStructures.SortedDict()
-
-for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)), readdir("$(PKG_BASE)/docs/src", join=true))
-    
-#     @show documentation_group
-    # cleanup all current .ipynb exports
-    for converted_notebook_file in filter(x -> occursin(r"\.ipynb\.md$", x), readdir(documentation_group, join=true))
-#         @show converted_notebook_file
-        rm(converted_notebook_file)
-    end
-    
+for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)), readdir("$(PKG_BASE)/docs/_src", join=true))
     # convert all current .ipynb notebooks to markdown files
     for notebook in filter(x -> occursin(r".ipynb$", x), readdir(documentation_group, join=true))
-#         @show notebook
-        out_path = notebook * ".md" 
+        out_path = joinpath("$(PKG_BASE)/docs/src", basename(documentation_group), basename(notebook) * ".md")
         Weave.weave(notebook, out_path=out_path, doctype="github")
-#         Weave.weave(notebook, doctype="github")
     end
     
+    # copy non notebook files over
+    for non_notebook in filter(x -> !occursin(r".ipynb$", x), readdir(documentation_group, join=true))
+        out_path = joinpath("$(PKG_BASE)/docs/src", basename(documentation_group), basename(non_notebook))
+        cp(non_notebook, out_path, force=true)
+    end
+end
+
+pages = DataStructures.SortedDict()
+for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)), readdir("$(PKG_BASE)/docs/src", join=true))
     md_pages = filter(x -> occursin(r"\.md", x), readdir(documentation_group))
-    md_pages = map(page -> "$(basename(documentation_group))/$(page)", md_pages)
-    
-#     @show md_pages
-    
+    md_pages = map(page -> "$(basename(documentation_group))/$(page)", md_pages)    
     pages[basename(documentation_group)] = md_pages
 end
 
@@ -51,7 +46,8 @@ makedocs(;
     repo="https://github.com/cjprybol/Mycelia/blob/{commit}{path}#L{line}",
     sitename="Mycelia.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+#         prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls=false,
         canonical="https://cjprybol.github.io/Mycelia.jl",
         assets=String[],
         collapselevel=1,
@@ -59,17 +55,8 @@ makedocs(;
     pages=pages
 )
 
-# makedocs(;
-#     modules=[Mycelia],
-#     authors="Cameron Prybol <cameron.prybol@gmail.com> and contributors",
-#     repo="https://github.com/cjprybol/Mycelia.jl/blob/{commit}{path}#L{line}",
-#     sitename="Mycelia.jl",
-#     format=Documenter.HTML(;
-#         prettyurls=get(ENV, "CI", "false") == "true",
-#         canonical="https://cjprybol.github.io/Mycelia.jl",
-#         assets=String[],
-#         collapselevel=1,
-#     ),
+
+# ADD ME!!!
 #     pages=[
 #         "Home" => "README.md",
 #         "Tutorial" => [
@@ -79,18 +66,7 @@ makedocs(;
 #             "Subsetting graphs",
 #             "Adding annotations to graphs",
 #             "Adding taxonomy to graphs"
-#         ],
-#         "Applications" => [
-#         ],
-#         "Discussion" => chapters,
-#         "Reference" => [
-#             "docstrings.md",
-#             "neo4j-notes.md"
-#         ],
-#         "Nesting" => [
-#             "nested" => []
 #         ]
-#     ],
 # )
 
 deploydocs(
@@ -101,9 +77,4 @@ deploydocs(
     devurl = "docs"
 )
 
-for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)), readdir("$(PKG_BASE)/docs/src", join=true))
-    # cleanup all current .ipynb exports
-    for converted_notebook_file in filter(x -> occursin(r"\.ipynb\.md$", x), readdir(documentation_group, join=true))
-        rm(converted_notebook_file)
-    end
-end
+# rm("$(PKG_BASE)/docs/src", recursive=true)
