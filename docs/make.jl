af427291386d99e6f8f682225b0401d545e82302
Cameron Prybol <cameron.prybol@gmail.com>
2021-12-17 02:41:28 +0000

    Cleanup documentation creation

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ import DataStructures
 PKG_BASE = dirname(dirname(pathof(Mycelia)))
 
 # copy readme from repo to being main index file in documentation
-cp("$(PKG_BASE)/README.md", "$(PKG_BASE)/docs/src/README.md", force=true)
+cp("$(PKG_BASE)/README.md", "$(PKG_BASE)/docs/src/index.md", force=true)
 # for svg in filter(x -> occursin(r"\.svg$", x), readdir(PKG_BASE, join=true))
 #     cp(svg, "$(PKG_BASE)/docs/src/$(basename(svg))", force=true)
 # end
@@ -19,7 +19,7 @@ for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)
     
     @show documentation_group
     # cleanup all current .ipynb exports
-    for converted_notebook_file in filter(x -> occursin(r"\.ipynb\.md", x), readdir(documentation_group, join=true))
+    for converted_notebook_file in filter(x -> occursin(r"\.ipynb\.md$", x), readdir(documentation_group, join=true))
         @show converted_notebook_file
         rm(converted_notebook_file)
     end
@@ -37,25 +37,27 @@ for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)
     
     @show md_pages
     
-#     pages[basename(documentation_group)] = 
+    pages[basename(documentation_group)] = md_pages
 end
 
-# makedocs(;
-#     modules=[Mycelia],
-#     authors="Cameron Prybol <cameron.prybol@gmail.com> and contributors",
-#     repo="https://github.com/cjprybol/Mycelia.jl/blob/{commit}{path}#L{line}",
-#     sitename="Mycelia.jl",
-#     format=Documenter.HTML(;
-#         prettyurls=get(ENV, "CI", "false") == "true",
-#         canonical="https://cjprybol.github.io/Mycelia.jl",
-#         assets=String[],
-#         collapselevel=1,
-#     ),
-#     pages=[
-#         "Home" => "README.md",
-#         collect(pages)...
-#     ],
-# )
+pages=[
+    "Home" => "index.md",
+    collect(pages)...
+]
+
+makedocs(;
+    modules=[Mycelia],
+    authors="Cameron Prybol <cameron.prybol@gmail.com> and contributors",
+    repo="https://github.com/cjprybol/Mycelia.jl/blob/{commit}{path}#L{line}",
+    sitename="Mycelia.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://cjprybol.github.io/Mycelia.jl",
+        assets=String[],
+        collapselevel=1,
+    ),
+    pages=pages
+)
 
 # makedocs(;
 #     modules=[Mycelia],
@@ -91,10 +93,17 @@ end
 #     ],
 # )
 
-# deploydocs(
-#     repo = "github.com/cjprybol/Mycelia.git",
-#     push_preview = true,
-#     deps = nothing,
-#     make = nothing,
-#     devurl = "docs"
-# )
+deploydocs(
+    repo = "github.com/cjprybol/Mycelia.git",
+    push_preview = true,
+    deps = nothing,
+    make = nothing,
+    devurl = "docs"
+)
+
+for documentation_group in filter(x -> isdir(x) && !occursin(r"^\.", basename(x)), readdir("$(PKG_BASE)/docs/src", join=true))
+    # cleanup all current .ipynb exports
+    for converted_notebook_file in filter(x -> occursin(r"\.ipynb\.md$", x), readdir(documentation_group, join=true))
+        rm(converted_notebook_file)
+    end
+end
