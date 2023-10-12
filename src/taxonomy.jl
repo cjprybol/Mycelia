b241cc641e157ee27c0b1cae59c9d9da12c60e64
Cameron Prybol <cameron.prybol@gmail.com>
2023-10-12 16:18:00 -0700

    streamline taxonomy functions

--- src/taxonomy.jl
+++ src/taxonomy.jl
@@ -16,7 +16,7 @@ function list_full_taxonomy()
         "genus",
         "species"
     ]
-    for rank inranks
+    for rank in ranks
         table[!, rank] .= ""
         table[!, "$(rank)_taxid"] .= ""
     end
@@ -47,98 +47,42 @@ function list_rank(rank)
     p = pipeline(
         `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
         `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "$(rank)"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{$(shorthand)}'`
+        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format "{$(shorthand)}"`
     )
     data, header = uCSV.read(open(p), delim='\t')
     header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return DataFrames.DataFrame(data, header)
 end
 
 
 function list_superkingdoms()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "superkingdom"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{k}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("superkingdom")
 end
 
 function list_kingdoms()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "kingdom"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{K}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("kingdom")
 end
 
 function list_phylums()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "phylum"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{p}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("phylum")
 end
 
 function list_classes()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "class"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{c}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("class")
 end
 
 function list_orders()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "order"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{o}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("order")
 end
 
 function list_families()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "family"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{f}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("family")
 end
 
 function list_genera()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "genus"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{g}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("genus")
 end
 
 function list_species()
-    p = pipeline(
-        `conda run --no-capture-output -n taxonkit taxonkit list --ids 1`,
-        `conda run --no-capture-output -n taxonkit taxonkit filter --equal-to "species"`,
-        `conda run --no-capture-output -n taxonkit taxonkit reformat --taxid-field 1 --format '{s}'`
-    )
-    data, header = uCSV.read(open(p), delim='\t')
-    header = ["taxid", "name"]
-    DataFrames.DataFrame(data, header)
+    return list_rank("species")
 end
