db5cba26626fd885d5d18f14c96d52c527e6c253
Cameron Prybol <cameron.prybol@gmail.com>
2024-10-22 21:13:31 +0000

    drop old build command

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,7 @@
 println("Building the package...")
 
 import Mycelia
-Mycelia.add_bioconda_envs()
+# Mycelia.add_bioconda_envs()
 
 # Add your installation-specific commands here
 # E.g., checking for external dependencies, downloading necessary files, etc.
