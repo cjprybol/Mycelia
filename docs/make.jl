f23e503a6cb2b23ff29543ef95455b193e8ae471
cjprybol <cjprybol@users.noreply.github.com>
2025-01-25 20:22:03 +0000

    update make to deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,28 @@
 using Documenter
 using Mycelia
 
-# julia --project=. docs/make.jl
-
+# format=Documenter.HTML(;
+# prettyurls=get(ENV, "CI", "false") == "true",
+# canonical="https://cjprybol.github.io/Mycelia",
+# assets=String[],
+# collapselevel=1,
+# ),
 makedocs(
-    sitename = "Mycelia Documentation",
+    sitename = "Mycelia",
     modules = [Mycelia],
+    authors="Cameron Prybol <cameron.prybol@gmail.com> and contributors",
+    repo="https://github.com/cjprybol/Mycelia/blob/{commit}{path}#L{line}",
     format = Documenter.HTMLWriter.HTML(size_threshold = 300_000),  # Increase the size threshold
     pages = [
         "Home" => "index.md",
         # Add other pages here
     ]
+)
+
+deploydocs(
+    repo = "github.com/cjprybol/Mycelia.git",
+    # push_preview = true,
+    # deps = nothing,
+    # make = nothing,
+    # devurl = "docs"
 )
